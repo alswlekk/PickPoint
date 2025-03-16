029933383186ae0cc7dea2970ee0f6ea80147a81
casperjr <hyunjw0515@gmail.com>
2025-03-16 16:49:41 +0900

    [doc]: 버전 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 2
-        versionName = "1.1"
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
