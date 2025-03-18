06198ef88a3127239f1688b80fe7a0a1bdd7dd27
casperjr <hyunjw0515@gmail.com>
2025-03-18 13:00:21 +0900

    [feat]: 터치된 점이 없으면 TapToStart를 보여주도록 구현

--- app/src/main/java/com/pickpoint/pickpoint/ui/whattodo/component/WTDGameComponent.kt
+++ app/src/main/java/com/pickpoint/pickpoint/ui/whattodo/component/WTDGameComponent.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.pickpoint.pickpoint.ui.common.component.CircleButton
+import com.pickpoint.pickpoint.ui.common.component.TapToStartComponent
 import com.pickpoint.pickpoint.ui.common.util.getPointColorList
 import com.pickpoint.pickpoint.ui.common.util.timerStartHandler
 import com.pickpoint.pickpoint.ui.theme.AppTheme
@@ -124,6 +125,14 @@ fun WTDGameComponent(
                 }
             }
     ) {
+        // 게임 시작 전 Tap to Start 표시
+        if (touchPoints.isEmpty() && isGameActive){
+            TapToStartComponent(
+                modifier = modifier
+                    .align(Alignment.Center)
+            )
+        }
+
         // 현재 활성화된 각 터치에 대해 Point composable 표시
         if (isGameActive) {
             touchPoints.forEach { (_, data) ->
