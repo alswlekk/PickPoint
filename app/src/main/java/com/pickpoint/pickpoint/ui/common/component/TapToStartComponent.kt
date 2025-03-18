e719dc3ef7209c742161809c1c6e80676cfb9939
casperjr <hyunjw0515@gmail.com>
2025-03-18 12:40:02 +0900

    [ui]: tap to start ui 구현

---
+++ app/src/main/java/com/pickpoint/pickpoint/ui/common/component/TapToStartComponent.kt
@@ -0,0 +1,42 @@
+package com.pickpoint.pickpoint.ui.common.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pickpoint.pickpoint.ui.theme.AppTheme
+import com.pickpoint.pickpoint.ui.theme.PickPointTheme
+
+@Composable
+fun TapToStartComponent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Tap to Start!",
+            color = MaterialTheme.colorScheme.onPrimary,
+            style = MaterialTheme.typography.displayLarge
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TapToStartComponentPreview() {
+    PickPointTheme(theme = AppTheme.LIGHT_PROTOTYPE, dynamicColor = false) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TapToStartComponent()
+        }
+    }
+}
