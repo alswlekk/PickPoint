889efd083c6acac6113e742d39ae6c43a1124826
casperjr <hyunjw0515@gmail.com>
2025-03-25 01:51:30 +0900

    [fix]: 일본어 taptostart 정렬 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pickpoint.pickpoint"
         minSdk = 29
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3.0"
+        versionCode = 6
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
